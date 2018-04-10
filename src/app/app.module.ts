e99347d9d641e0990c78efd2852b3ed9466c4d27
Pauline Bravo <pauline.bravo@keleo-solutions.com>
2018-04-10 12:01:14 +0200

    project is existing

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
