6989c9177ee8dba6b85a986b67554b27c53c760a
Pauline Bravo <pauline.bravo@keleo-solutions.com>
2018-04-11 16:09:44 +0200

    Creation of coin service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 import { appRoutes } from './routerConfig';
-import {HttpClientModule} from '@angular/common/http';
+import { CoinService } from './coin.service';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,7 @@ import {HttpClientModule} from '@angular/common/http';
     RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
-  providers: [],
+  providers: [CoinService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
