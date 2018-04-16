17363cd463d772b08410da6cb0c824d5a168b390
Pauline Bravo <pauline.bravo@keleo-solutions.com>
2018-04-16 11:40:37 +0200

    Form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 import { appRoutes } from './routerConfig';
+
 import { CoinService } from './coin.service';
 
 
@@ -21,7 +24,8 @@ import { CoinService } from './coin.service';
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [CoinService],
   bootstrap: [AppComponent]
